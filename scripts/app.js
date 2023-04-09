87a5082e04fcea2862a138bbac617b32467d7f54
Kamil Raza <m.kamilrazabhatti@gmail.com>
2023-04-09 12:08:31 +0500

    FEAT:JS 1 to 9 Chap

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+// DAY 1 - ( 9 APR)
+
+alert("Hello")
+document.write("<h1>Hellos</h1>")
+console.log("Hellos")
+prompt("Your Name:","Usama")
+
+var mumtazPagal //camelCase
+var MumtazSmjhdar //PascalCase
+var mumtaz_chala_jaa_bhai //snake_case
+
+var username = prompt("Your Name:")
+var halChaal = "Kese ho Bhai"
+alert("Greet " + username +" "+ halChaal)
+
+var num = "3"
+console.log(typeof num)
+
+var myAge = +prompt("Your Age")
+var FAge = +prompt("Your Fathers Age")
+console.log(myAge + FAge)
+
+
+console.log(2+4)
+console.log(2-4)
+console.log(2*4)
+console.log(2/4)
+console.log(2**4)
+
+
+var num = 1
+// console.log(num)
+// num = num +1
+console.log(num++)
+
+
+console.log(((1+3)/6)*((59/5)+((9*45)/78)))
